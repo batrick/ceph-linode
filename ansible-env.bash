e135071186a1badd510eaa4fda31fddb5990c820
Ben England <bengland@redhat.com>
2017-10-12 08:03:14 -0400

    avoid excessive -vvv logging, pretty-print JSON

--- ansible-env.bash
+++ ansible-env.bash
@@ -1,9 +1,11 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_INVENTORY=ansible_inventory
 export ANSIBLE_SSH_RETRIES=20
+# pretty-print JSON to prevent logging hairballs
+export ANSIBLE_STDOUT_CALLBACK=debug
 
 SSH_COMMON_ARGS="-o ConnectTimeout=60 -o ConnectionAttempts=10 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-ANSIBLE_ARGS="--timeout=60 -vvv --forks=50 --become"
+ANSIBLE_ARGS="--timeout=60 -vv --forks=50 --become"
 
 function repeat {
     while ! "$@"; do
