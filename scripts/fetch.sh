e95fa4005016ca2018215235037fa44c58317ab9
Patrick Donnelly <pdonnell@redhat.com>
2017-10-14 10:47:48 -0700

    cleanup
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -21,9 +21,8 @@ function get {
 }
 
 function fetch_host {
-    #         ceph-osd-14 ansible_ssh_host=50.116.48.233 ansible_ssh_port=22 ansible_ssh_user='root' ansible_ssh_private_key_file='/home/pdonnell/.ssh/id_rsa'
-    local host="$(cut -f1 -d' ' <<<"$1")"
-    local ipaddr="$(grep --only-matching '[[:digit:]]\+\.[[:digit:]]\+.[[:digit:]]\+.[[:digit:]]\+' <<< "$1")"
+    local ipaddr="$1"
+    local host="$2"
 
     run ssh -i ~/ansible.id_rsa -o PreferredAuthentications=publickey -o ConnectTimeout=60 -o ConnectionAttempts=10 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@${ipaddr}" 'gzip /var/log/ceph/*.log /crash/*.core'
     run mkdir -p -m 755 ./logs/"$host" ./stats/"$host" ./crash/"$host"
@@ -36,12 +35,14 @@ run mkdir -p -m 755 ./crash
 run mkdir -p -m 755 ./logs
 run mkdir -p -m 755 ./stats
 
-grep 'mds-[[:digit:]]\+ ansible_ssh_host=' ansible_inventory | (while read line; do
-    run queue_task fetch_host "$line"
-done; wait)
+function fetch_group {
+    < linodes jq -rc ".[] | select(.name | contains(\"$1\")) | .ip_public, .name" | (while read ip; do
+        read name
+        run queue_task fetch_host "$ip" "$name"
+    done; wait)
+}
 
-grep 'client-[[:digit:]]\+ ansible_ssh_host=' ansible_inventory | (while read line; do
-    run queue_task fetch_host "$line"
-done; wait)
+fetch_group mds
+fetch_group client
 
 wait
