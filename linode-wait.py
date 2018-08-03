ea22b0b77c83398afc8c91ead914258525790a1e
Ben England <bengland@redhat.com>
2018-08-03 16:53:44 -0400

    no GROUP, linode_api4, python3, ipv6

--- linode-wait.py
+++ linode-wait.py
@@ -1,28 +1,38 @@
+#!/usr/bin/env python
 import binascii
 import logging
 import os
 import time
+import subprocess
+import linode_api4
 
-import linode.api as linapi
+homedir = os.getenv('HOME')
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-with open("LINODE_GROUP") as f:
-    GROUP = unicode(f.read())
+def wait(keyfn):
+    with open(keyfn, 'r') as kf:
+        key = kf.readline().strip()
+    client = linode_api4.LinodeClient(key)
+    linodes = [ l for l in client.linode.instances() ]
 
-def wait(key):
-    client = linapi.Api(key = key, batching = False)
-
-    notdone = True
-    while notdone:
-        linodes = client.linode_list()
-        print(linodes)
-        notdone = False
-        for linode in linodes:
-            if linode[u'LPM_DISPLAYGROUP'] == GROUP:
-                if linode[u'STATUS'] != 1:
-                    notdone = True
-        time.sleep(5)
+    linodes_not_up = linodes[:]
+    while len(linodes) > 0:
+        for l in linodes:
+            ipv6addr = l.ipv6.split('/')[0]
+            logging.info('see if %s is up' % ipv6addr)
+            try:
+                output = subprocess.check_output(['ssh', '-o StrictHostKeyChecking=no', '-6', ipv6addr, 'pwd'])
+                if not str(output).__contains__(homedir):
+                    logging.warn('failed to reach %s' % ipv6addr)
+                else:
+                    linodes_not_up.remove(l)
+            except subprocess.CalledProcessError as e:
+                logging.warn(str(e))
+        logging.info('waiting 2 seconds before retrying')
+        if len(linodes_not_up) > 0:
+            time.sleep(2)
+        linodes = linodes_not_up[:]
 
 def main():
     key = os.getenv("LINODE_API_KEY")
