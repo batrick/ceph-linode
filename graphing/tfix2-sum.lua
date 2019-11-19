45ffe0bb35a9e00ef0691fad8e65ad617af1a105
Patrick Donnelly <pdonnell@redhat.com>
2019-11-18 21:46:48 -0800

    graphing: fix mds-requests-type for multimds
    tfix2 probably never worked for multimds. Not sure what I was thinking
    there.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

---
+++ graphing/tfix2-sum.lua
@@ -0,0 +1,90 @@
+local mds = {}
+local min_timestamp = math.maxinteger
+local max_timestamp = 0
+
+local null = setmetatable({}, {__index = function(t,k) return 0 end})
+local mt = {__index = function(t, k) return null end}
+for line in io.lines() do
+    local timestamp, rank, data = line:match "(%d+)\t(%d+)%s*(.*)"
+    rank = math.tointeger(rank)
+    if not rank then io.stderr:write(line, "\n") os.exit(1) end
+    timestamp = math.tointeger(timestamp)
+    --mds[rank] = mds[rank] or {timestamps=setmetatable({}, mt),last=math.maxinteger-1,first=timestamp}
+    if mds[rank] == nil then
+        mds[rank] = {
+            timestamps=setmetatable({}, mt),
+            last=math.maxinteger-1,
+            first=timestamp,
+            n=nil,
+        }
+    end
+    local rank = mds[rank]
+    local last = rank.last
+    local timestamps = rank.timestamps
+    local datas = {}
+    timestamps[timestamp] = datas
+    -- io.stderr:write(tostring(timestamp), " ", data, '\n')
+    for d in data:gmatch("(%d+)%s*") do
+        local d = tonumber(d)
+        local n = #datas+1
+        datas[n] = d
+        --io.stderr:write('\t', tostring(n), "=", tostring(d), '\n')
+        if last+1 <= timestamp-1 then
+            local step = (d-timestamps[last][n])//(timestamp-last)
+            for i = last+1, timestamp-1 do
+                if rawget(timestamps, i) == nil then
+                    timestamps[i] = {}
+                end
+                --io.stderr:write("last=", tostring(last), " i=", tostring(i), '\n')
+                --io.stderr:write(tostring(timestamps[i]), " ", tostring(timestamps[i-1]), '\n')
+                --io.stderr:write(tostring(timestamps[i][n]), " ", tostring(timestamps[i-1][n]), '\n')
+                timestamps[i][n] = timestamps[i-1][n]+step
+            end
+        end
+    end
+    if rank.n == nil then
+        rank.n = #datas
+    else
+        assert(#datas == rank.n)
+    end
+    rank.last = timestamp
+    min_timestamp = math.min(min_timestamp, timestamp)
+    max_timestamp = math.max(max_timestamp, timestamp)
+end
+
+--[[
+local mt = {__index = function() return 0 end}
+for rank, mds in pairs(mds) do
+    for i = min_timestamp, mds.first do
+        mds.timestamps[i] = setmetatable({}, mt)
+    end
+    for i = mds.last, max_timestamp do
+        mds.timestamps[i] = setmetatable({}, mt)
+    end
+end
+--]]
+
+local ranks = {}
+local columns = 0
+for rank, mds in pairs(mds) do
+    ranks[#ranks+1] = rank
+    if columns == 0 then columns = mds.n end
+    assert(mds.n == columns)
+end
+table.sort(ranks)
+
+for i = min_timestamp, max_timestamp do
+    io.write(i-min_timestamp)
+    for n = 1, columns do
+        local v = 0
+        for id, rank in ipairs(ranks) do
+            local mds = mds[rank]
+            local timestamps = mds.timestamps[i]
+            local d = timestamps[n]
+            --io.stderr:write(i, '\t', _, '\t', d, '\n')
+            v = v+d
+        end
+        io.write('\t', v)
+    end
+    io.write '\n'
+end
